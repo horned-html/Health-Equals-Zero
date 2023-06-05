cfc0ec2c62569fbd9aef9b01baae974c4aea79d9
Amber <jayhrod26@gmail.com>
2023-06-05 18:13:50 -0400

    Update HEZ.cpp

--- HEZ.cpp
+++ HEZ.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!\n";
+    bool done = false;
+
+    string input = "";
+
+    int wizardHealth = 100;
+    int playerHealth = 100;
+
+    cout << "\n\tHealth Equals Zero\n\n";
+    while (!done) {
+        cout << "Wizard HP: " << wizardHealth << ".\n";
+        cout << "Player HP: " << playerHealth << ".\n";
+        cout << "Enter X to exit.\n";
+        cin >> input;
+        if (input == "X" or input == "x") {
+            done = true;
+        }
+    }
+    return 0;
 }
